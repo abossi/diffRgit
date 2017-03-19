ce60a17f23ab62bc22ad321b3b048350d08828b0
abossi <bossi.adrien@gmail.com>
2017-03-19 16:05:42 +0100

    make modification for a potential cran package

--- R/diffRgit.R
+++ R/diffRgit.R
@@ -7,6 +7,9 @@
 #' @param outputFormat 'side-by-side' or 'line-by-line'
 #' @param showFiles boolean to display or not the header with files changed
 #' @param matching 'lines' or 'words'
+#' @param width width of the widget
+#' @param height height of the widget
+#' @param elementId ID of the widget div
 #'
 #' @note All parameters are directly send to diff2html function to be compatible with futures versions.
 #'
@@ -15,7 +18,7 @@
 #' @examples
 #' library(diffRgit)
 #'
-#' #get git diff with system('git diff file1 file2", intern = T) instead
+#' #get git diff with system('git diff file1 file2", intern = TRUE) instead
 #' diff <- "diff --git a/README.md b/README.md\r\nindex 132c8a2..46909f2 100644\r\n--- a/README.md\r\n+++ b/README.md\r\n@ -98,6 +98,9 @ The HTML output accepts a Javascript object with configuration. Possible options\r\n   - `synchronisedScroll`: scroll both panes in side-by-side mode: `true` or `false`, default is `false`\r\n   - `matchWordsThreshold`: similarity threshold for word matching, default is 0.25\r\n   - `matchingMaxComparisons`: perform at most this much comparisons for line matching a block of changes, default is `2500`\r\n+  - `templates`: object with previously compiled templates to replace parts of the html\r\n+  - `rawTemplates`: object with raw not compiled templates to replace parts of the html\r\n+  > For more information regarding the possible templates look into [src/templates](https://github.com/rtfpessoa/diff2html/tree/master/src/templates)\r\n \r\n ## Diff2HtmlUI Helper"
 #' diffRgit(diff)
 #'
@@ -23,7 +26,7 @@
 #' diffRgit(diff, outputFormat = 'line-by-line')
 #'
 #' #remove header
-#' diffRgit(diff, showFiles = F)
+#' diffRgit(diff, showFiles = FALSE)
 #'
 #' @import htmlwidgets
 #'
